12e667efab244ebeb76cf8608fbda77c29ddb42d
ChromeWax <richardphong1@gmail.com>
2024-09-16 17:32:59 -0400

    train_model should work now

--- train_model.py
+++ train_model.py
@@ -3,7 +3,6 @@ from functools import partial
 import pandas as pd
 import numpy as np
 import random
-import multiprocessing
 import math
 import json
 from tqdm.auto import tqdm
@@ -100,6 +99,9 @@ def tensor_to_pil(tensor):
     transform = transforms.ToPILImage()
     img = transform(tensor)
     return img
+
+def custom_collate_fn(batch):
+    return tuple(zip(*batch))
 
 def run_epoch(model, dataloader, optimizer, lr_scheduler, device, scaler, epoch_id, is_training):
     """
@@ -236,208 +238,206 @@ def train_loop(model,
     if device.type != 'cpu':
         getattr(torch, device.type).empty_cache()
 
-'''
-These are specific paths for dataset, checkpoints, and other files
-'''
-# Path for dataset
-dataset_path = Path("./Images")
-image_file_paths = list(dataset_path.glob("*.jpg"))
-annotation_file_paths = list(dataset_path.glob("*.json"))
+if __name__ ==  "__main__":
+    '''
+    These are specific paths for dataset, checkpoints, and other files
+    '''
+    # Path for dataset
+    dataset_path = Path("./Images")
+    image_file_paths = list(dataset_path.glob("*.jpg"))
+    annotation_file_paths = list(dataset_path.glob("*.json"))
 
-# Name of the font file
-font_file = 'KFOlCnqEu92Fr1MmEU9vAw.ttf'
+    # Name of the font file
+    font_file = 'KFOlCnqEu92Fr1MmEU9vAw.ttf'
 
-'''
-This sets up the device and datatype
-'''
-# Gets device for training
-device = get_torch_device()
-dtype = torch.float32
+    '''
+    This sets up the device and datatype
+    '''
+    # Gets device for training
+    device = get_torch_device()
+    dtype = torch.float32
 
-'''
-This grabs the dataset and creates a dataframe for them
-'''
-# Dictionary to map each file name to path
-image_dict = {file.stem : file for file in image_file_paths}
+    '''
+    This grabs the dataset and creates a dataframe for them
+    '''
+    # Dictionary to map each file name to path
+    image_dict = {file.stem : file for file in image_file_paths}
 
-# Dataframe for annotations
-cls_dataframes = (pd.read_json(file_path, orient='index').transpose() for file_path in annotation_file_paths)
-annotation_df = pd.concat(cls_dataframes, ignore_index=False)
-annotation_df['index'] = annotation_df.apply(lambda row: row['imagePath'].split('.')[0], axis=1)
-annotation_df = annotation_df.set_index('index')
+    # Dataframe for annotations
+    cls_dataframes = (pd.read_json(file_path, orient='index').transpose() for file_path in annotation_file_paths)
+    annotation_df = pd.concat(cls_dataframes, ignore_index=False)
+    annotation_df['index'] = annotation_df.apply(lambda row: row['imagePath'].split('.')[0], axis=1)
+    annotation_df = annotation_df.set_index('index')
 
-# Dataframe for segmentations of annotations
-shapes_df = annotation_df['shapes'].explode().to_frame().shapes.apply(pd.Series)
+    # Dataframe for segmentations of annotations
+    shapes_df = annotation_df['shapes'].explode().to_frame().shapes.apply(pd.Series)
 
-# Gets unique list of classes, in this case just one
-class_names = shapes_df['label'].unique().tolist()
+    # Gets unique list of classes, in this case just one
+    class_names = shapes_df['label'].unique().tolist()
 
-# The Mask R-CNN model expects datasets to have a background class
-class_names = ['background'] + class_names
+    # The Mask R-CNN model expects datasets to have a background class
+    class_names = ['background'] + class_names
 
-# Generate a list of colors with a length equal to the number of labels
-colors = distinctipy.get_colors(len(class_names))
-int_colors = [tuple(int(c*255) for c in color) for color in colors]
+    # Generate a list of colors with a length equal to the number of labels
+    colors = distinctipy.get_colors(len(class_names))
+    int_colors = [tuple(int(c*255) for c in color) for color in colors]
 
-draw_bboxes = partial(draw_bounding_boxes, fill=False, width=2, font=font_file, font_size=25)
+    draw_bboxes = partial(draw_bounding_boxes, fill=False, width=2, font=font_file, font_size=25)
 
-'''
-This loads the Mask R-CNN Model 
-'''
-# Initialize a Mask R-CNN model with pretrained weights
-model = maskrcnn_resnet50_fpn_v2(weights='DEFAULT')
+    '''
+    This loads the Mask R-CNN Model 
+    '''
+    # Initialize a Mask R-CNN model with pretrained weights
+    model = maskrcnn_resnet50_fpn_v2(weights='DEFAULT')
 
-# Get the number of input features for the classifier
-in_features_box = model.roi_heads.box_predictor.cls_score.in_features
-in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    # Get the number of input features for the classifier
+    in_features_box = model.roi_heads.box_predictor.cls_score.in_features
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
 
-# Get the numbner of output channels for the Mask Predictor
-dim_reduced = model.roi_heads.mask_predictor.conv5_mask.out_channels
+    # Get the numbner of output channels for the Mask Predictor
+    dim_reduced = model.roi_heads.mask_predictor.conv5_mask.out_channels
 
-# Replace the box predictor
-model.roi_heads.box_predictor = FastRCNNPredictor(in_channels=in_features_box, num_classes=len(class_names))
+    # Replace the box predictor
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_channels=in_features_box, num_classes=len(class_names))
 
-# Replace the mask predictor
-model.roi_heads.mask_predictor = MaskRCNNPredictor(in_channels=in_features_mask, dim_reduced=dim_reduced, num_classes=len(class_names))
+    # Replace the mask predictor
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_channels=in_features_mask, dim_reduced=dim_reduced, num_classes=len(class_names))
 
-# Set the model's device and data type
-model.to(device=device, dtype=dtype);
+    # Set the model's device and data type
+    model.to(device=device, dtype=dtype);
 
-# Add attributes to store the device and model name for later reference
-model.device = device
-model.name = 'maskrcnn_resnet50_fpn_v2'
+    # Add attributes to store the device and model name for later reference
+    model.device = device
+    model.name = 'maskrcnn_resnet50_fpn_v2'
 
-'''
-This splits the available data into training and validation data
-'''
-# Get list of image IDs
-image_keys = list(image_dict.keys())
+    '''
+    This splits the available data into training and validation data
+    '''
+    # Get list of image IDs
+    image_keys = list(image_dict.keys())
 
-# Shuffles IDs
-random.shuffle(image_keys)
+    # Shuffles IDs
+    random.shuffle(image_keys)
 
-# Split the subset of image paths into training and validation sets
-train_percentage = 0.8
-train_keys = image_keys[ : int(len(image_keys) * train_percentage)]
-valid_keys = image_keys[int(len(image_keys) * train_percentage) : ]
+    # Split the subset of image paths into training and validation sets
+    train_percentage = 0.8
+    train_keys = image_keys[ : int(len(image_keys) * train_percentage)]
+    valid_keys = image_keys[int(len(image_keys) * train_percentage) : ]
 
-'''
-This creates additional training and validation data
-'''
-data_augment_transforms = transforms.Compose(
-    transforms=[
-        transforms.ColorJitter(
-                brightness = (0.875, 1.125),
-                contrast = (0.5, 1.5),
-                saturation = (0.5, 1.5),
-                hue = (-0.05, 0.05),
-        ),
-        transforms.RandomGrayscale(),
-        transforms.RandomEqualize(),
-        transforms.RandomPosterize(bits=3, p=0.5),
-        transforms.RandomHorizontalFlip(p=0.5),
-        transforms.RandomVerticalFlip(p=0.5),
-    ],
-)
+    '''
+    This creates additional training and validation data
+    '''
+    data_augment_transforms = transforms.Compose(
+        transforms=[
+            transforms.ColorJitter(
+                    brightness = (0.875, 1.125),
+                    contrast = (0.5, 1.5),
+                    saturation = (0.5, 1.5),
+                    hue = (-0.05, 0.05),
+            ),
+            transforms.RandomGrayscale(),
+            transforms.RandomEqualize(),
+            transforms.RandomPosterize(bits=3, p=0.5),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomVerticalFlip(p=0.5),
+        ],
+    )
 
-# Compose transforms to sanitize bounding boxes and normalize input data
-final_tranforms = transforms.Compose([
-    transforms.ToImage(), 
-    transforms.ToDtype(torch.float32, scale=True),
-    transforms.SanitizeBoundingBoxes(),
-])
+    # Compose transforms to sanitize bounding boxes and normalize input data
+    final_tranforms = transforms.Compose([
+        transforms.ToImage(), 
+        transforms.ToDtype(torch.float32, scale=True),
+        transforms.SanitizeBoundingBoxes(),
+    ])
 
-# Compose transforms to resize and pad input images
-resize_tranforms = transforms.Compose([
-    transforms.Resize([640, 480], antialias=True)
-])
+    # Compose transforms to resize and pad input images
+    resize_tranforms = transforms.Compose([
+        transforms.Resize([640, 480], antialias=True)
+    ])
 
-# Define the transformations for training and validation datasets
-train_tfms = transforms.Compose([
-    data_augment_transforms, 
-    resize_tranforms,
-    final_tranforms
-])
-valid_tfms = transforms.Compose([
-    resize_tranforms,
-    final_tranforms
-])
+    # Define the transformations for training and validation datasets
+    train_tfms = transforms.Compose([
+        data_augment_transforms, 
+        resize_tranforms,
+        final_tranforms
+    ])
+    valid_tfms = transforms.Compose([
+        resize_tranforms,
+        final_tranforms
+    ])
 
-'''
-Initialize Datasets
-'''
-# Create a mapping from class names to class indices
-class_to_idx = {c: i for i, c in enumerate(class_names)}
+    '''
+    Initialize Datasets
+    '''
+    # Create a mapping from class names to class indices
+    class_to_idx = {c: i for i, c in enumerate(class_names)}
 
-# Instantiate the datasets using the defined transformations
-train_dataset = BaseballCardDataset(train_keys, annotation_df, image_dict, class_to_idx, train_tfms)
-valid_dataset = BaseballCardDataset(valid_keys, annotation_df, image_dict, class_to_idx, valid_tfms)
+    # Instantiate the datasets using the defined transformations
+    train_dataset = BaseballCardDataset(train_keys, annotation_df, image_dict, class_to_idx, train_tfms)
+    valid_dataset = BaseballCardDataset(valid_keys, annotation_df, image_dict, class_to_idx, valid_tfms)
 
-'''
-Initialize Dataloaders
-'''
-# Set the training batch size
-bs = 4
+    '''
+    Initialize Dataloaders
+    '''
+    # Set the training batch size
+    bs = 4
 
-# Set the number of worker processes for loading data.
-num_workers = multiprocessing.cpu_count()//2
+    # Define parameters for DataLoader
+    data_loader_params = {
+        'batch_size': bs,  # Batch size for data loading
+        'num_workers': 4,  # Number of subprocesses to use for data loading
+        'persistent_workers': True,  # If True, the data loader will not shutdown the worker processes after a dataset has been consumed once. This allows to maintain the worker dataset instances alive.
+        'pin_memory': 'cuda' in device,  # If True, the data loader will copy Tensors into CUDA pinned memory before returning them. Useful when using GPU.
+        'pin_memory_device': device if 'cuda' in device else '',  # Specifies the device where the data should be loaded. Commonly set to use the GPU.
+        'collate_fn': custom_collate_fn,
+    }
 
-# Define parameters for DataLoader
-data_loader_params = {
-    'batch_size': bs,  # Batch size for data loading
-    'num_workers': num_workers,  # Number of subprocesses to use for data loading
-    'persistent_workers': True,  # If True, the data loader will not shutdown the worker processes after a dataset has been consumed once. This allows to maintain the worker dataset instances alive.
-    'pin_memory': 'cuda' in device,  # If True, the data loader will copy Tensors into CUDA pinned memory before returning them. Useful when using GPU.
-    'pin_memory_device': device if 'cuda' in device else '',  # Specifies the device where the data should be loaded. Commonly set to use the GPU.
-    'collate_fn': lambda batch: tuple(zip(*batch)),
-}
+    # Create DataLoader for training data. Data is shuffled for every epoch.
+    train_dataloader = DataLoader(train_dataset, **data_loader_params, shuffle=True)
 
-# Create DataLoader for training data. Data is shuffled for every epoch.
-train_dataloader = DataLoader(train_dataset, **data_loader_params, shuffle=True)
+    # Create DataLoader for validation data. Shuffling is not necessary for validation data.
+    valid_dataloader = DataLoader(valid_dataset, **data_loader_params)
 
-# Create DataLoader for validation data. Shuffling is not necessary for validation data.
-valid_dataloader = DataLoader(valid_dataset, **data_loader_params)
+    '''
+    Set Model Checkpoint
+    '''
+    # Create a directory to store the checkpoints if it does not already exist
+    checkpoint_directory = Path("./Checkpoints")
+    # The model checkpoint path
+    checkpoint_path = checkpoint_directory/f"{model.name}.pth"
 
-'''
-Set Model Checkpoint
-'''
-# Create a directory to store the checkpoints if it does not already exist
-checkpoint_directory = Path("./Checkpoints")
-# The model checkpoint path
-checkpoint_path = checkpoint_directory/f"{model.name}.pth"
+    '''
+    Saves Color Map
+    '''
+    # Create a color map and write it to a JSON file
+    color_map = {'items': [{'label': label, 'color': color} for label, color in zip(class_names, colors)]}
+    with open(f"{checkpoint_directory}/{dataset_path.name}-colormap.json", "w") as file:
+        json.dump(color_map, file)
 
-'''
-Saves Color Map
-'''
-# Create a color map and write it to a JSON file
-color_map = {'items': [{'label': label, 'color': color} for label, color in zip(class_names, colors)]}
-with open(f"{checkpoint_directory}/{dataset_path.name}-colormap.json", "w") as file:
-    json.dump(color_map, file)
+    '''
+    Configures training parameters
+    '''
+    # Learning rate for the model
+    lr = 5e-4
 
-'''
-Configures training parameters
-'''
-# Learning rate for the model
-lr = 5e-4
+    # Number of training epochs
+    epochs = 40
 
-# Number of training epochs
-epochs = 40
+    # AdamW optimizer; includes weight decay for regularization
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
 
-# AdamW optimizer; includes weight decay for regularization
-optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+    # Learning rate scheduler; adjusts the learning rate during training
+    lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=lr, total_steps=epochs*len(train_dataloader))
 
-# Learning rate scheduler; adjusts the learning rate during training
-lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=lr, total_steps=epochs*len(train_dataloader))
-
-'''
-Trains Model
-'''
-train_loop(model=model, 
-        train_dataloader=train_dataloader,
-        valid_dataloader=valid_dataloader,
-        optimizer=optimizer, 
-        lr_scheduler=lr_scheduler, 
-        device=torch.device(device), 
-        epochs=epochs, 
-        checkpoint_path=checkpoint_path,
-        use_scaler=True)
+    '''
+    Trains Model
+    '''
+    train_loop(model=model, 
+            train_dataloader=train_dataloader,
+            valid_dataloader=valid_dataloader,
+            optimizer=optimizer, 
+            lr_scheduler=lr_scheduler, 
+            device=torch.device(device), 
+            epochs=epochs, 
+            checkpoint_path=checkpoint_path,
+            use_scaler=True)
